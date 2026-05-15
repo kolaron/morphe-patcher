0a748b302728bf8ccaff5adf25000676a9c8c137
Ondřej Kolář <ondra@kolar.pm>
2026-05-15 08:57:50 +0200

    feat: Remove entire ad view

--- patches/src/main/kotlin/app/morphe/patches/idos/RemoveAdsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/idos/RemoveAdsPatch.kt
@@ -18,5 +18,17 @@ val disableAdsPatch = bytecodePatch(
             return-void
             """.trimIndent()
         )
+        LoadNativeAdInvokeSuspendFingerprint.method.addInstructions(
+            0,
+            $$"""
+            const/16 v0, 0x8
+            invoke-virtual {p0, v0}, Landroid/view/View;->setVisibility(I)V
+            invoke-virtual {p0}, Landroid/view/View;->getLayoutParams()Landroid/view/ViewGroup$LayoutParams;
+            move-result-object v0
+            const/4 v1, 0x0
+            iput v1, v0, Landroid/view/ViewGroup$LayoutParams;->height:I
+            return-void
+            """
+        )
     }
 }
